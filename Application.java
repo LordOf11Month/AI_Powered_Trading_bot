2f174ccf62dc580df1654659eaefcc0174021e52
Ramazan Secilmis <ramazan.secilmis15@gmail.com>
2025-11-23 09:58:47 +0300

    Add .env_template for environment variable setup, .gitignore to exclude .env, and create Application.java as entry point to Application

---
+++ Application.java
@@ -0,0 +1,18 @@
+//enter point for application 
+public class Application {
+    
+    public static void main(String[] args) {
+        
+    }
+
+
+    /*usefull method when you try to test specific part of the application and lets you run your code from here
+    Usage:add this method to main, put break point to this method, write your test case like unit testing, use built in debugger. 
+    */
+    public static void TestingGround(){
+    }
+
+    /* the initalization and starting of the application will be done here*/
+    public static void init(){}
+
+}
